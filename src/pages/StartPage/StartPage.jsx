ee23e2f29ca96c4cfc7d33b573ac855893e807ea
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-22 11:15:25 +0200

    Create StartPage.jsx

---
+++ src/pages/StartPage/StartPage.jsx
@@ -0,0 +1,7 @@
+import TextField from '@mui/material/Button';
+
+export const StartPage =()=> {
+    return (            
+        <TextField  style={{width:700,height:300,fontSize:20}} id="outlined-basic" label="Outlined" variant="outlined">Hello!We are glad to welcome you to the 'Book of Contacts'  application. Here you can store contacts of your friends, relatives and partners. Register to use the application. And if you are already with us, then enter your username and password to enter the application.</TextField>             
+    )
+}
