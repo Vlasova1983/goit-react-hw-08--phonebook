27863735e66cb21e35b2753ce114da8ce32204aa
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-22 11:14:33 +0200

    Create http.js

---
+++ src/http/http.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const publicApi = axios.create({
+  baseURL: 'https://connections-api.herokuapp.com',
+});
+
+export const privateApi = axios.create({
+  baseURL: 'https://connections-api.herokuapp.com',
+});
+
+export const token = {
+  set: (data) => {   
+    privateApi.defaults.headers.Authorization =`${data.token}` ;
+  },
+
+  remove: () => {
+    privateApi.defaults.headers.Authorization = null;
+  },
+};
