be7b85ed7d96a18b05149325371fc693e04c1e34
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-22 11:14:28 +0200

    Create UseMenu.jsx

---
+++ src/components/UseMenu/UseMenu.jsx
@@ -0,0 +1,29 @@
+import styles from "../UseMenu/UseMenu.module.css"
+
+import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+
+import {selectProfile} from '../../redux/profile/profile.selector';
+import {logoutThunk} from "../../redux/logout/logout.thunk";
+import InputIcon from '@mui/icons-material/Input';
+
+export const UseMenu =()=> {  
+  const location = useLocation();  
+  const profile = useSelector(selectProfile);
+  
+  const dispatch = useDispatch();     
+
+  const handleLogout = () =>{                          
+    dispatch(logoutThunk());
+  };
+
+  return (    
+    <div className={styles.flex}>     
+      <p style={{margin:0}}>{profile.email}</p>      
+      <NavLink style={{marginTop:30,color:"#11a4ed",fontSize:20}} onClick={handleLogout} to={location.state?.from?? '/'} replace>
+        <InputIcon></InputIcon>
+      </NavLink>         
+    </div>         
+  );     
+};
