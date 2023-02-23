a2d4a6884a8525fda9f1b97df365ab707037b25d
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-23 21:30:13 +0200

    Update auth.thunk.js

--- src/redux/auth/auth.thunk.js
+++ src/redux/auth/auth.thunk.js
@@ -1,8 +1,29 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { token, publicApi } from '../../http/http';
+import { token, privateApi } from '../../http/http';
+import { selectAuthToken } from '../auth/auth.selector';
+
 
 export const authLoginThunk = createAsyncThunk('login', async(value)=>{
-  const {data} = await publicApi.post('/users/login', value);  
+  const {data} = await privateApi.post('/users/login', value);  
   token.set(data);
+  return data;})
+
+
+export const getProfileThunk = createAsyncThunk('current', async (_, { getState, rejectWithValue }) => {
+  const stateToken = selectAuthToken(getState());
+
+  if (!stateToken) {
+    return rejectWithValue();
+  }
+
+  token.set(stateToken);
+  const { data } = await privateApi.get('/users/current'); 
+  return data;
+}); 
+
+export const logoutThunk = createAsyncThunk('logout', async(_, { getState })=>{
+  const {data} = await privateApi.post('/users/logout'); 
+  getState(null);
+  token.remove(data); 
   return data;
 })
