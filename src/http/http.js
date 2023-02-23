8ce694c93da7f9397d435732af9c22532456a348
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-23 21:29:46 +0200

    Update http.js

--- src/http/http.js
+++ src/http/http.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-export const publicApi = axios.create({
-  baseURL: 'https://connections-api.herokuapp.com',
-});
-
 export const privateApi = axios.create({
   baseURL: 'https://connections-api.herokuapp.com',
 });
