1fb54a73fe4697200b2b02e1e60bb8b7a8ec60c0
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-23 21:29:49 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,26 @@
 import { useEffect } from "react";
 import { useDispatch,useSelector} from "react-redux";
 
+
 import { ContactForm } from '../../components/ContactForm/ContactForm';
 import { Filter } from '../../components/Filter/Filter';
 import { ContactList } from '../../components/ContactList/ContactList';
 import { UseMenu } from '../../components/UseMenu/UseMenu';
 
-import { getError, getIsLoading } from "../../redux/contacts/contacts.selectors";
+
 import { selectAuthToken } from "../../redux/auth/auth.selector";
-import{selectProfile} from '../../redux/profile/profile.selector';
+import{selectProfile} from '../../redux/auth/auth.selector';
+
 import { fetchContact } from "../../redux/contacts/contacts.thunk";
+import { getError, getIsLoading } from "../../redux/contacts/contacts.selectors";
 
 export const HomePage  =()=> {  
-  const dispatch = useDispatch();
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);  
+  const dispatch = useDispatch();  
   const profile = useSelector(selectProfile);  
   const token = useSelector(selectAuthToken);
-  
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);  
+
   useEffect(() => {      
     dispatch(fetchContact());     
   }, [ dispatch]);  
@@ -35,8 +38,8 @@ export const HomePage  =()=> {
             </div>             
           </div>          
           <div>
-            {isLoading && !error && <b>Request in progress....</b>}    
-          <ContactList/>   
+          {isLoading && !error && <b>Request in progress....</b>}
+          <ContactList/>         
           </div>           
         </div>       
       </div>                   
