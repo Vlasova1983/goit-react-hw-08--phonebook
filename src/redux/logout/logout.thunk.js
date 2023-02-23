bef15947f0606de41ae20c10f39099c6538a4765
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-23 21:30:44 +0200

    Delete logout.thunk.js

--- src/redux/logout/logout.thunk.js
+++
@@ -1,9 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { token, privateApi } from '../../http/http';
-
-
-export const logoutThunk = createAsyncThunk('logout', async()=>{
-  const {data} = await privateApi.post('/users/logout');  
-  token.remove(data); 
-  return data;
-})
