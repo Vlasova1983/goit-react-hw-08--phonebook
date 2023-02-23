5d165cc17cce47c9d787c20c70453d8acac492dc
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-23 21:30:58 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,35 +9,17 @@ import {
   REGISTER,
 } from 'redux-persist';
 
-import { contactInitState } from './contacts/contacts.init-state';
-import { filterInitState } from './filter/filter.init-state';
-import {authtInitState} from "./auth/auth.init-state";
-import { profileInitState } from './profile/profile.init-state';
-import {logoutInitState} from "./logout/logout.init-state";
-
 import { contactsReducer } from '../redux/contacts/contacts.slice';
-import { filterReducer } from './filter/filter.slice';
+import {filterReducer} from './contacts/filter.slice';
 import { authReducer } from './auth/auth.slice';
-import { profileReducer } from './profile/profile.slice';
-import {logoutReducer} from './logout/logout.slice';
 
-const initState = { 
-  contacts: contactInitState.contacts,
-  filter:filterInitState,
-  auth:authtInitState,
-  profile: profileInitState,
-  logout: logoutInitState,
-};
 
-export const store = configureStore({
-  preloadedState: initState,
+export const store = configureStore({  
   devTools: true,
   reducer: { 
-    auth: authReducer, 
-    profile: profileReducer,   
-    contacts: contactsReducer, 
-    filter: filterReducer, 
-    logout:logoutReducer,
+    contacts: contactsReducer,
+    filter: filterReducer,
+    auth: authReducer,  
   },
 
   middleware: getDefaultMiddleware =>
