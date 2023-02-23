1fb981dbc1ae08da12be2900340b54d0db538a45
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-23 21:30:10 +0200

    Update auth.slice.js

--- src/redux/auth/auth.slice.js
+++ src/redux/auth/auth.slice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { authtInitState } from "./auth.init-state";
 import { persistReducer } from 'redux-persist';
-import {authLoginThunk} from './auth.thunk';
-
+import {authLoginThunk,getProfileThunk,logoutThunk} from './auth.thunk';
+import {STATUS} from "../../contents/status.contents";
 import storage from 'redux-persist/lib/storage';
 
 const authSlice = createSlice({
@@ -13,12 +13,29 @@ const authSlice = createSlice({
   },
   extraReducers: builder => {
     builder.addCase(authLoginThunk.pending, state => {
-      state.status = 'loading';
+      state.status = STATUS.loading;
     }).addCase(authLoginThunk.fulfilled, (state, { payload }) => {
-      state.status = 'success';
-      state.data = payload;
+      state.status = STATUS.success;
+      state.auth = payload; 
+      state.isLoggedIn = true;     
     }).addCase(authLoginThunk.rejected, state => {
-      state.status = 'error';
+      state.status = STATUS.error;
+    }).addCase(getProfileThunk.pending, (state) => {
+      state.status = STATUS.loading;
+    }).addCase(getProfileThunk.fulfilled, (state, { payload }) => {
+      state.status = STATUS.success;
+      state.profile = payload; 
+      state.isLoggedIn = true;     
+    }).addCase(getProfileThunk.rejected, (state) => {
+      state.status = STATUS.error;
+    }).addCase(logoutThunk.pending, state => {
+      state.status = STATUS.loading;
+    }).addCase(logoutThunk.fulfilled, (state, { payload }) => {
+      state.status = STATUS.success;
+      state.logout = payload; 
+      state.isLoggedIn = false;    
+    }).addCase(logoutThunk.rejected, state => {
+      state.status = STATUS.error;
     });
   },
 });
@@ -27,7 +44,7 @@ export const { logoutAction } = authSlice.actions;
   
 export const authReducer = persistReducer({
   key: 'auth',
-  storage,
+  storage,   
 }, authSlice.reducer);
 
 
